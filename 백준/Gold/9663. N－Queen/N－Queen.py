a1b940cfbf652bb7e1f56cc0ac638b3e1916d736
JunOnJuly <wlgnstjd0717@gmail.com>
2023-11-27 19:15:01 +0900

    fix comde

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,24 +1,39 @@
-def is_empty(x):
-    for i in range(x):
-        if queen[x] == queen[i] or abs(queen[x] - queen[i]) == abs(x - i):
+import sys
+
+
+# 인덱스에 배치 가능한지 판단하는 함수
+def is_empty(row):
+    # 가로는 할 필요 없음
+    # 세로, 대각선
+    for i in range(row):
+        if queen[row] == queen[i] or abs(queen[i] - queen[row]) == abs(i - row):
             return False
     return True
 
 
-def n_queens(x):
-    global cnt
-    if x == n:
+# NQueen 재귀 함수
+def n_queen(row):
+    # 마지막 인덱스를 넘어가면
+    if row == N:
+        global cnt
         cnt += 1
         return
     else:
-        for i in range(n):
-            queen[x] = i
-            if is_empty(x):
-                n_queens(x+1)
-                
-                
-n = int(input())
+        # 인덱스를 돌며 탐색
+        for i in range(N):
+            # 인덱스에 퀸 배치
+            queen[row] = i
+            # 인덱스에 배치 가능하면
+            if is_empty(row):
+                # 재귀 호출
+                n_queen(row+1)
+
+
+N = int(sys.stdin.readline())
+# 카운트
 cnt = 0
-queen = [0] * n
-n_queens(0)
+# 퀸 인덱스
+queen = [0] * N
+# 함수 호출
+n_queen(0)
 print(cnt)
