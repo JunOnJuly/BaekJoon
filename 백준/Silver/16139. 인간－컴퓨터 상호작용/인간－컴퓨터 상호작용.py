eab5a3c390e3111a34f0db8587cf5973588c8209
성지훈 <97649331+JunOnJuly@users.noreply.github.com>
2023-11-28 18:22:16 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 476 ms, Memory: 195324 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,12 +1,25 @@
-S = list(input())
-q = int(input())
+import sys
+from collections import defaultdict
 
-for _ in range(q):
-    a, l, r = input().split()
-    l, r = map(int, [l, r])
 
-    answer = 0
-    for i in range(l, r + 1):
-        if S[i] == a:
-            answer += 1
-    print(answer)
+def solution(S, data_list):
+    # 문자별 누적합
+    subsum = [[0 for _ in range(len(S)+1)] for __ in range(26)]
+    # 문자열 순회 및 누적합 리스트 완성
+    for idx_S in range(len(S)):
+        for idx_subsum in range(len(subsum)):
+            # 해당 문자면 해당 문자 누적합의 해당 인덱스 값 + 1
+            if idx_subsum == ord(S[idx_S])-97:
+                subsum[idx_subsum][idx_S+1] = subsum[idx_subsum][idx_S] + 1
+            # 아니면 그대로
+            else:
+                subsum[idx_subsum][idx_S+1] = subsum[idx_subsum][idx_S]
+    # 데이터 순회 
+    for now_str, start, end in data_list:
+        print(subsum[ord(now_str)-97][int(end)+1] - subsum[ord(now_str)-97][int(start)])
+                
+        
+S = sys.stdin.readline().strip()
+q = int(sys.stdin.readline().strip())
+data_list = [sys.stdin.readline().strip().split() for _ in range(q)]
+solution(S, data_list)
