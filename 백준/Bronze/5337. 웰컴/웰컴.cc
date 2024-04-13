e70e2312af7548e5ea9333c25e2f8308062bff02
성지훈 <97649331+JunOnJuly@users.noreply.github.com>
2024-04-13 23:40:58 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (void){
+    string str_1 = ".  .   .";
+    string str_2 = "|  | _ | _. _ ._ _  _";
+    string str_3 = "|/\\|(/.|(_.(_)[ | )(/.";
+    
+    cout<<str_1<<"\n"<<str_2<<"\n"<<str_3;
+}
