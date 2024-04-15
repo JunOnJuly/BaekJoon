a45e2879f4f091bdcd5e52cfd318d858ba4aad0a
성지훈 <97649331+JunOnJuly@users.noreply.github.com>
2024-04-15 18:22:43 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    while(true){
+        int A, B;
+        cin>>A>>B;
+        if (A == 0 and B == 0) return 0;
+        cout<<A+B<<"\n";
+    }
+}
