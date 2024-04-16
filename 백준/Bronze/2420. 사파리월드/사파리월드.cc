c9a981fb6ebdcd2816f069778b1798c1c413fefe
성지훈 <97649331+JunOnJuly@users.noreply.github.com>
2024-04-16 18:47:55 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+int main(void){
+	long long int N, M;
+	cin >> N >> M;
+	
+	cout << abs(N - M);
+}
