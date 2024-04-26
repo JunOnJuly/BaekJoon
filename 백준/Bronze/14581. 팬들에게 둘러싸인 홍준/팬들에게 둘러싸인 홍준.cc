e622fd54eadca14546c2739b25d0b3691a30216f
성지훈 <97649331+JunOnJuly@users.noreply.github.com>
2024-04-26 22:07:07 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    string name;
+    cin>>name;
+    cout<<":fan::fan::fan:"<<"\n"<<":fan::"<<name<<"::fan:"<<"\n"<<":fan::fan::fan:";
+}
