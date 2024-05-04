7610affe205bc8bff488515b53e4d9fa8599b182
성지훈 <97649331+JunOnJuly@users.noreply.github.com>
2024-05-04 20:39:06 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    char alpha;
+    cin>>alpha;
+    if (alpha == 'N' or alpha == 'n') cout<<"Naver D2";
+    else cout<<"Naver Whale";
+}
