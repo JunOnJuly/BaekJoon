f14a17bbbc57d8136979793814aa23225b647edf
성지훈 <97649331+JunOnJuly@users.noreply.github.com>
2024-12-08 14:10:57 +0900

    [Silver V] Title: 막대기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,13 +1,1 @@
-import sys
-
-input = sys.stdin.readline
-
-
-def solution(X):
-    print(bin(X)[2:].count('1'))
-
-
-# 입력
-X = int(input().strip())
-
-solution(X)
+print(bin(int(input()))[2:].count('1'))
